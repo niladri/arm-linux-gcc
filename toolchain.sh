39eab5190e0fb04a05bb7d0f891465aeb94d2fe9
niladri83 <niladri83@bcbc8984-619b-11de-8c10-5fe6993ea61f>
2009-06-25 18:21:58 +0000

    Added commands for installation of linux headers

--- toolchain.sh
+++ toolchain.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 TOPLEVELDIR=`pwd`
-TARGET=cortex
+TARGET=arm-linux
+ARCH=arm
+PREFIX=$TOPLEVELDIR/cortex
 
 GCC=gcc-4.4.0
 BINUTILS=binutils-2.19.1
@@ -18,7 +20,7 @@ wget -c http://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2 \
 mkdir -pv build/$BINUTILS
 mkdir -pv build/$GCC
 mkdir -pv build/$GLIBC
-mkdir -pv $TARGET
+mkdir -pv $PREFIX
 
 if [ -d $BINUTILS ]
 then
@@ -37,7 +39,7 @@ if [ -f Makefile ]
 then
 	echo "Using old configuration. Delete build/$BINUTILS/Makefile to configure again"
 else
-	$TOPLEVELDIR/$BINUTILS/configure --target=arm-linux --prefix=$TOPLEVELDIR/$TARGET --disable-nls
+	$TOPLEVELDIR/$BINUTILS/configure --target=$TARGET --prefix=$PREFIX --disable-nls
 fi
 make
 make install
@@ -51,4 +53,17 @@ else
 	echo "Extracting $KERNEL.tar.bz2"
 	tar -jxf $KERNEL.tar.bz2
 fi
+
+# Installation of Linux Headers
+mkdir -pv $PREFIX/usr/include
+cd $KERNEL
+make mrproper
+make ARCH=$ARCH headers_check
+make ARCH=$ARCH INSTALL_HDR_PATH=tmp_dest headers_install
+cp -rv tmp_dest/include/* $PREFIX/usr/include
+rm -rf tmp_dest
+cd $TOPLEVELDIR
+
+# Extract and Build Cross GCC with static libgcc and no threads
+# TODO
 
