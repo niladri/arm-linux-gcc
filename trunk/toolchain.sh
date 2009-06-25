b0e263ac447c7951e4c660bc92b1540933efdcdf
niladri83 <niladri83@bcbc8984-619b-11de-8c10-5fe6993ea61f>
2009-06-25 19:44:23 +0000

    Building GCC requires GMP 4.1+ and MPFR 2.3.2+ Added links to source packages for GMP and MPFR

--- trunk/toolchain.sh
+++ trunk/toolchain.sh
@@ -10,18 +10,24 @@ BINUTILS=binutils-2.19.1
 GLIBC=glibc-2.9
 GLIBCLT=glibc-linuxthreads-2.5
 KERNEL=linux-2.6.30
+GMP=gmp-4.3.1
+MPFR=mpfr-2.4.1
 
+# Download source packages
 wget -c http://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2 \
 		http://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2 \
 		http://ftp.gnu.org/gnu/glibc/$GLIBC.tar.bz2 \
 		http://ftp.gnu.org/gnu/glibc/$GLIBCLT.tar.bz2 \
-		http://www.kernel.org/pub/linux/kernel/v2.6/$KERNEL.tar.bz2 
+		http://ftp.gnu.org/gnu/gmp/$GMP.tar.bz2 \
+		http://www.mpfr.org/mpfr-current/$MPFR.tar.bz2 \
+		http://www.kernel.org/pub/linux/kernel/v2.6/$KERNEL.tar.bz2
 
 mkdir -pv build/$BINUTILS
 mkdir -pv build/$GCC
 mkdir -pv build/$GLIBC
 mkdir -pv $PREFIX
 
+# Extract binutils
 if [ -d $BINUTILS ]
 then
 	echo "$BINUTILS exists"
@@ -34,6 +40,7 @@ fi
 # http://sourceware.org/bugzilla/show_bug.cgi?id=7026
 sed -i 's/as_bad (_(r/as_bad ("%s", _(r/' $BINUTILS/gas/config/tc-arm.c
 
+# Build and install binutils
 cd build/$BINUTILS
 if [ -f Makefile ]
 then
@@ -43,9 +50,9 @@ else
 fi
 make
 make install
-
 cd $TOPLEVELDIR
 
+# Extract Linux kernel
 if [ -d $KERNEL ]
 then
 	echo "$KERNEL exists"
@@ -65,5 +72,24 @@ rm -rf tmp_dest
 cd $TOPLEVELDIR
 
 # Extract and Build Cross GCC with static libgcc and no threads
-# TODO
+
+# Extract GCC
+if [ -d $GCC ]
+then 
+	echo "$GCC exists"
+else
+	echo "Extracting $GCC.tar.bz2"
+	tar -jxf $GCC.tar.bz2
+fi
+
+# Build and install GCC
+cd build/$GCC
+if [ -f Makefile ]
+then
+	echo "Using old configuration. Delete build/$BINUTILS/Makefile to configure again"
+else
+	$TOPLEVELDIR/$GCC/configure --target=$TARGET --build=$MACHTYPE --prefix=$PREFIX --host=$MACHTYPE --target=$TARGET --with-sysroot=$PREFIX --disable-nls --disable-shared --disable-threads --enable-languages=c
+fi
+make
+make install
 
